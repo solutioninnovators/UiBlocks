8aa17e23d3d8882576fdf51037b45184722ed77d
thetuningspoon <mike@thetuningspoon.com>
2018-02-15 10:52:24 -0500

    Numerous updates and optimizations, support for nested file structure in ui folder

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-# MarkupModularUI - A Hierarchical Model-View-Controller (HMVC) Framework for building modular, nestable graphical user interfaces in ProcessWire.
+# UI Blocks - A Hierarchical Model-View-Controller (HMVC) Framework for building modular, nestable graphical user interfaces in ProcessWire.
 Developed by Mike Spooner (thetuningspoon) for Solution Innovators
 
 ## Usage
 Move the contents of the "Move to your templates folder" directory to /site/templates/ and set the alternate filename for each of your templates to main.php in the PW Admin.
 
-See files for documentation. More coming soon! 
-
+See files for documentation. More coming soon!
--- UI.php
+++ UI.php
@@ -21,22 +21,70 @@
 abstract class UI extends Wire {
 
 	protected $view; // The view file (HTML markup/template) this user interface will use for output
-	public $version = 1; // Update the version number of a user interface to force browsers to reload cached js & css assets
+	protected $ajax = array(); // An ajax data array to output instead of the view for ajax requests
+	public $id; // A unique identifier for the particular instance of the UI (useful mainly for AJAX calls)
+ 	public $version; // Update the version number of a user interface to force browsers to reload cached js & css assets
 	public $path = ''; // Path to the UI directory
 	public $url = ''; // URL to the UI directory
+	public $uiName = '';
 	public $headScripts = array();
 	public $footScripts = array();
 	public $styles = array();
 	public $minify = true;
+	public $wrapper = true; // Enable/disable the header and footer markup surrounding the block
+	public $classes = ''; // Classes to add to the wrapper
+	public $debug = null; // Allows AJAX data to be output even if $config->ajax is false. May be used to switch on other debug data as needed. If not set, the value from PW's global $config->debug will be used
+	public $allowAjaxConstruct = false; // Allows the UI to be constructed by an ajax call on any page before any other controllers are executed. See line 60 of UiBlocks-master for more information
 
-	function __construct() {
+	function __construct(array $options = null) {
+		// Copy values from the options array to public properties
+		if($options) {
+			foreach($options as $key => $val) {
+				if(property_exists($this, $key)) {
+					$this->$key = $val;
+				}
+			}
+		}
+
 		$this->path = $this->getPath();
 		$this->url = $this->getUrl();
+		$this->uiName = $this->getUiName();
+
+		$this->view = new TemplateFile();
+		$viewFileName = $this->path . $this->uiName . '.php';
+		if(file_exists($viewFileName)) $this->view->setFilename($viewFileName); // Load the default view file for this controller, which is expected to be in the same folder as the controller file
 
 		$this->setup();
-		if(!$this->view) {
-			$this->view = new TemplateFile(dirname($this->getClassFile())."/{$this->getUiName()}.php"); // Load the default view file for this controller, which is expected to be in the same folder as the controller file
-		}
+
+		$this->autoIncludeAssets();
+		if($this->minify && $this->modules->isInstalled("AllInOneMinify")) $this->minify();
+		$this->passAssets();
+	}
+
+
+	/*public function addHeadScript($file) {
+		$this->headScripts[] = $file;
+		$this->modules->UiBlocks->headScripts[] = $file;
+	}
+
+	public function addFootScript($file) {
+		$this->footScripts[] = $file;
+		$this->modules->UiBlocks->footScripts[] = $file;
+	}
+
+	public function addStyle($file) {
+		$this->styles[] = $file;
+		$this->modules->UiBlocks->styles[] = $file;
+	}*/
+
+	/**
+	 * Factory for building UI objects. This is an alternative to using "new" directly. Allows for more compact code.
+	 * @param $options - Configuration options to send to the UI's constructor function.
+	 */
+	public static function build($UIname, array $options = array()) {
+		if(substr($UIname, -2) != 'UI') $UIname = $UIname . 'UI';
+		$UIname = '\\ProcessWire\\' . $UIname;
+		return new $UIname($options);
 	}
 
 	/**
@@ -45,8 +93,17 @@ abstract class UI extends Wire {
 	 */
 	protected function setup() {}
 
-	public function getUiName() {
-		$nameParts = explode('\\', get_class($this));
+	/**
+	 * @param string $className Optional name of class to get the UI name from (uses the current class otherwise)
+	 * @return string The name of the current UI, without the namespace or "UI" suffix
+	 */
+	public function getUiName($className = null) {
+		if(!$className && $this->uiName) {
+			return $this->uiName;
+		}
+
+		if(!$className) $className = $this->className();
+		$nameParts = explode('\\', $className);
 		$className = array_pop($nameParts);
 		$uiName = substr($className, 0, -2); // Remove "UI" from the class name to get just the name of the user interface
 		return $uiName;
@@ -55,8 +112,9 @@ abstract class UI extends Wire {
 	/**
 	 * Returns the file name of the current class, even if this is a child class. This is a workaround for the limitations of __FILE__
 	 */
-	public function getClassFile() {
-		$reflection = new \ReflectionClass($this);
+	public function getClassFile($className = null) {
+		$class = $className ?: $this;
+		$reflection = new \ReflectionClass($class);
 		$file = $reflection->getFileName();
 		return $file;
 	}
@@ -64,15 +122,25 @@ abstract class UI extends Wire {
 	/**
 	 * Returns the path of the current UI directory
 	 */
-	public function getPath() {
-		return dirname($this->getClassFile()) . '/';
+	public function getPath($className = null) {
+		if(!$className && $this->path) {
+			return $this->path;
+		}
+
+        return str_replace('\\', '/', dirname($this->getClassFile($className)))  . '/';
 	}
 
 	/**
 	 * Returns the URL of the current UI directory
 	 */
-	public function getUrl() {
-		return $this->config->urls->templates . "ui/" . $this->getUiName() . "/";
+	public function getUrl($className = null) {
+		if(!$className && $this->url) {
+			return $this->url;
+		}
+
+		$path = $className ? $this->getPath($className) : $this->getPath();
+		$pathAfterTemplatesFolder = str_replace($this->config->paths->templates, '', $path);
+		return $this->config->urls->templates . $pathAfterTemplatesFolder;
 	}
 
 	/**
@@ -99,25 +167,44 @@ abstract class UI extends Wire {
 	/**
 	 * If there is a JS or CSS file with this UI's namesake, include it (if it isn't already added)
 	 */
-	protected function autoIncludeAssets() {
-		$versionSuffix = $this->version ? "?v={$this->version}" : '';
+	private function autoIncludeAssets() {
 
-		$fileName = "{$this->getUiName()}.js";
-		$filePath = $this->path . $fileName;
-		$fileUrl = $this->url . $fileName;
-
-		if(file_exists($filePath) && !in_array($fileUrl, $this->headScripts) && !in_array($fileUrl, $this->footScripts)) {
-			if(!$this->minify) $fileUrl .= $versionSuffix; // The AIOM module can't handle query strings
-			$this->headScripts[] = $fileUrl;
+		// Include any assets from parent UI classes first
+		foreach(class_parents($this, false) as $parentClassName) {
+			$parentClass = new \ReflectionClass($parentClassName);
+			if($parentClass->isSubclassOf("\\ProcessWire\\UI")) {
+				$this->autoIncludeAsset('js', 'headScripts', $parentClassName);
+				$this->autoIncludeAsset('css', 'styles', $parentClassName);
+			}
+			else break;
 		}
 
-		$fileName = "{$this->getUiName()}.css";
-		$filePath = $this->path . $fileName;
-		$fileUrl = $this->url . $fileName;
+		$this->autoIncludeAsset('js', 'headScripts');
+		$this->autoIncludeAsset('css', 'styles');
+	}
 
-		if(file_exists($filePath) && !in_array($fileUrl, $this->styles)) {
-			if(!$this->minify) $fileUrl .= $versionSuffix; // The AIOM module can't handle query strings
-			$this->styles[] = $fileUrl;
+	private function autoIncludeAsset($ext, $destinationArray, $className = null) {
+		$fileName = $this->getUiName($className) . '.' . $ext;
+		$filePath = $this->getPath($className) . $fileName;
+		$fileUrl = $this->getUrl($className) . $fileName;
+
+		$exists = false;
+		if($ext == 'js') {
+			if(file_exists($filePath) && !in_array($fileUrl, $this->headScripts) && !in_array($fileUrl, $this->footScripts))
+				$exists = true;
+		}
+		elseif($ext == 'css') {
+			if(file_exists($filePath) && !in_array($fileUrl, $this->styles)) {
+				$exists = true;
+			}
+		}
+
+		if($exists) {
+			if(!$this->minify || !$this->modules->isInstalled("AllInOneMinify") && $this->version) {
+				$versionSuffix = $this->version ? "?v={$this->version}" : '';
+				$fileUrl .= $versionSuffix; // The AIOM module can't handle query strings
+			}
+			$this->$destinationArray[] = $fileUrl;
 		}
 	}
 
@@ -157,7 +244,7 @@ abstract class UI extends Wire {
 	}
 
 	/**
-	 * @todo: for each path in $this->headScripts, if starts with http:// or https://, add to $this->layout->headScripts directly
+	 * @return Array of assets with absolute paths (cannot be minified)
 	 */
 	protected function getExternalAssets($array, $location) {
 		$output = array();
@@ -175,8 +262,6 @@ abstract class UI extends Wire {
 				if ( gettype( $val ) === 'string' )
 					$output[] = $val;
 			}
-
-
 		}
 
 		return $output;
@@ -184,13 +269,12 @@ abstract class UI extends Wire {
 	}
 
 	/**
-	 * Passes all local scripts and styles to the layout (outermost UI) for inclusion in the html
-	 * @todo: Should this be modified to use $config, or make the layout object configurable?
+	 * Passes all local scripts and styles to the global assets arrays for inclusion in the html
 	 */
-	protected function passAssetsToLayout() {
-		$this->layout->headScripts = array_unique(array_merge($this->layout->headScripts, $this->headScripts));
-		$this->layout->footScripts = array_unique(array_merge($this->layout->footScripts, $this->footScripts));
-		$this->layout->styles      = array_unique(array_merge($this->layout->styles,      $this->styles));
+	protected function passAssets() {
+		$this->modules->UiBlocks->headScripts = array_unique(array_merge($this->modules->UiBlocks->headScripts, $this->headScripts));
+		$this->modules->UiBlocks->footScripts = array_unique(array_merge($this->modules->UiBlocks->footScripts, $this->footScripts));
+		$this->modules->UiBlocks->styles      = array_unique(array_merge($this->modules->UiBlocks->styles,      $this->styles));
 	}
 
 	protected function minify() {
@@ -219,17 +303,88 @@ abstract class UI extends Wire {
 	protected function run() {}
 
 	/**
-	 * Called by the client to run the user interface and output its markup
+	 * Called after the view is rendered. Child classes can use this to execute any controller logic post-view.
 	 */
-	public function output() {
-		$this->autoIncludeAssets();
-		if($this->minify && $this->modules->isInstalled("AllInOneMinify")) $this->minify();
-		$this->passAssetsToLayout();
-		$this->run();
+	protected function end() {}
+
+	/**
+	 * @return string to include before the contents of the view (feel free to override in subclasses)
+	 */
+	protected function header() {
+		$name = $this->getUiName();
+		$id = $this->sanitizer->entities($this->id);
+		$uiId = empty($id) ? '' : "ui_$id";
+		$htmlId = empty($id) ? '' : "id='ui_$id'";
+
+		return "<div $htmlId class='ui ui_$name $uiId {$this->sanitizer->entities($this->classes)}' data-ui-name='$name' data-ui-id='$id'>";
+	}
+
+	/**
+	 * @return string to include after the contents of the view (feel free to override in subclasses)
+	 */
+	protected function footer() {
+		return "</div>";
+	}
+
+	/**
+	 * Passes variables to the view and calls render() on it
+	 * @return string output from view TemplateFile
+	 */
+	protected function renderView() {
+		$this->view->ui = $this; // Pass the UI object to the view
 		$this->passWireVarsToView();
 		$this->passPublicPropertiesToView();
 
-		return $this->view->render();
+		$output = '';
+		if($this->wrapper) $output .= $this->header();
+		$output .= $this->view->render();
+		if($this->wrapper) $output .= $this->footer();
+		return $output;
+	}
+
+	/**
+	 * Called by the client to run the user interface controller logic and output its markup (or ajax array, if this is an ajax call)
+	 */
+	public function output() {
+		if($this->debug === null) $this->debug = $this->config->debug; // If debug mode is not set, use PW's global debug setting
+
+		if($this->input->ajax && ($this->config->ajax || $this->debug === true) && ($this->input->ui == $this->id || $this->input->ui == $this->getUIName()) && method_exists($this, 'ajax_' . $this->input->ajax)) {
+			$method = 'ajax_' . $this->input->ajax;
+			$this->$method();
+			$this->outputAjaxData();
+		}
+		else { // Render the view
+			$this->run();
+			$output = $this->renderView();
+			$this->end();
+
+			return $output;
+		}
+	}
+
+	/**
+	 * Alias for output() @todo: rename output() to run(), renderView() to output(), point this alias to output(), and rename run() to main()?
+	 */
+	public function render() {
+		return $this->output();
+	}
+
+	/**
+	 * Directly outputs the UI's data array in json format and halts further program execution (Used for AJAX calls)
+	 * @todo: rename to outputAjax()
+	 */
+	protected function outputAjaxData() {
+		header('Content-Type: application/json');
+		echo json_encode($this->ajax); exit;
+	}
+
+	/**
+	 * Built-in ajax function to return the view of any UI
+	 * Produces JSON array with single "view" element containing the html markup
+	 */
+	protected function ajax_reload() {
+		$this->run();
+		$this->ajax['view'] = $this->renderView();
 	}
 
 }
